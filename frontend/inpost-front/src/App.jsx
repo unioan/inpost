66db85d1a972eeab672ee1dcebd4e6a2e3c3d178
unioan <v.yushkov@yahoo.com>
2025-06-20 08:16:20 +0500

    frontend: add redirect to landing if user is not logged in

--- frontend/inpost-front/src/App.jsx
+++ frontend/inpost-front/src/App.jsx
@@ -1,17 +1,43 @@
-import { useEffect } from 'react';
-import { useFetchUser } from './hooks/useFetchUser';
+import { useState, useEffect, useRef } from 'react';
+import { checkAuthentication } from './services/api';
 import Dashboard from './pages/Dashboard';
+import Landing from './pages/Landing';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const [userId, setUserId] = useFetchUser({});
+  const [isAuthenticated, SetIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const hasCheckedRef = useRef(false);
 
   useEffect(() => {
-    (async () => {
-      await setUserId('jepe', '123321');
-    })();
-  }, []);
+    if (hasCheckedRef.current) return;
+    hasCheckedRef.current = true;
 
-  return <Dashboard />;
+    (async () => {
+      try {
+        const result = await checkAuthentication();
+        console.log('DEBUG: isAuthenticated', result);
+        SetIsAuthenticated(result);
+        setLoading(false);
+        if (result) {
+          navigate('/dashboard');
+        }
+      } catch (error) {
+        const { code } = error.response.data;
+        if (code == 401) {
+          SetIsAuthenticated(false);
+          setLoading(false);
+        }
+      }
+    })();
+  }, [navigate]);
+
+  if (loading) {
+    return <div></div>;
+  }
+
+  return <Landing />;
 }
 
 export default App;
---
+++ frontend/inpost-front/src/pages/Landing.jsx
@@ -0,0 +1,5 @@
+function Landing() {
+  return <div>dratuite</div>;
+}
+
+export default Landing;
